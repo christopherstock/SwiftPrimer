e314ecd048a0bfb231fce58338c319e26bcd4ca7
Christopher Stock <email@christopherstock.de>
2018-10-24 18:23:17 +0200

    Added deinitializer.

--- SwiftPrimer/classes/SwiftPrimer.swift
+++ SwiftPrimer/classes/SwiftPrimer.swift
@@ -12,28 +12,33 @@ class SwiftPrimer
     {
         self.name = name
     }
-    
+
+    deinit
+    {
+        print( "Instance of 'SwiftPrimer' being destroyed!" )
+    }
+
     static func test() -> Void
     {
         let mySwiftPrimer: SwiftPrimer = SwiftPrimer( name: "Chrisy" )
-        
+
         // try the SWIFT syntax tour
         mySwiftPrimer.trySwiftSyntaxTour()
-        
+
         // try the greet example
         print( mySwiftPrimer.greet( person: "Bob", on: "Tuesday", 27 ) )
         print( "" )
-        
+
         // invoke a static method
         SwiftPrimer.printMyString( myString: "The string to print out .." )
         print( "" )
-        
+
         // invoke external class and get my tuple
         let myTuple : (name:String, age:Int, gender:String) = SwiftPrimer.getMyTuple()
         print( "My Tuple is: " )
         print( myTuple )
         print( "" )
-        
+
         // show instance and static field
         print( "My instance field is: " + String( mySwiftPrimer.myInstanceField ) )
         print( "My name is: " + mySwiftPrimer.name )
@@ -53,7 +58,7 @@ class SwiftPrimer
             gender: "Male"
         )
     }
-    
+
     /**
      *  Tries all primal SWIFT syntax features.
      */
@@ -62,27 +67,27 @@ class SwiftPrimer
         print( ""                         )
         print( "========================" )
         print( "Test code START"          )
-        
+
         let MY_CONSTANT : String = "42"
         var myVariable  : String = "77"
-        
+
         let MY_CONSTANT_2 : Int = 7
         var myVariable2   : Int = 67
-        
+
         let MY_CONSTANT_3 : Double = 7.3
         var myVariable3   : Double = 67.2
-        
+
         let MY_CONSTANT_4 : Float = 77.0
         var myVariable4   : Float = 69.0
-        
+
         let MY_CONSTANT_5 : Int! = Int( MY_CONSTANT )
-        
+
         let MY_LONG_STRING : String = """
 This is my line 1.
 This is my line 2.
 This is my line 3.
 """;
-        
+
         let MY_ARRAY_1 : [String] = [
             "Chris",
             "Eugen",
@@ -97,9 +102,9 @@ This is my line 3.
         let MY_LIST_2 : [String:Int] = [String:Int]()
         let MY_ARRAY_3 : [String] = []
         let MY_LIST_3 : [String:Int] = [:]
-        
+
         myVariable = "65"
-        
+
         print( "My constant  is: "         + MY_CONSTANT             );
         print( "My variable  is: "         + myVariable              );
         print( "My variable2 is: "         + String( myVariable2   ) );
@@ -108,7 +113,7 @@ This is my line 3.
         print( "My long string is: "       + MY_LONG_STRING          );
         print( "My array is: "             + MY_ARRAY_1.description  );
         print( "My list is: "              + MY_LIST_1.description   );
-        
+
         if ( true )
         {
             print( "Condition always granted 1" )
@@ -126,25 +131,25 @@ This is my line 3.
         {
             print( "Condition never granted 2" )
         }
-        
+
         print( "Team members:" )
         for member : String in MY_ARRAY_1
         {
             print( " Team member: " + member )
         }
-        
+
         print( "Team ages one:" )
         for ( name, age ) in MY_LIST_1
         {
             print( " Team member [" + name + "] age [" + String( age ) + "]" )
         }
-        
+
         print( "Team ages two:" )
         for ( member ) in MY_LIST_1
         {
             print( " Team member [" + member.key + "] age [" + String( member.value ) + "]" )
         }
-        
+
         print( "Switch test:" )
         var myVariable5 : Int = 42;
         switch ( myVariable5 )
@@ -161,7 +166,7 @@ This is my line 3.
         default:
             print( " Switch case default" )
         }
-        
+
         var i : Int = 0
         print( "Loop test headed" )
         while ( i < 10 )
@@ -169,7 +174,7 @@ This is my line 3.
             print( " Loop value [" + String( i ) + "]" )
             i += 1
         }
-        
+
         var j : Int = 0
         print( "Loop test footed" )
         repeat
@@ -178,25 +183,25 @@ This is my line 3.
             j += 1
         }
             while ( j < 10 )
-        
+
         print( "Range in For Loop ONE" )
         for k : Int in 0..<5
         {
             print( " Number is [" + String( k ) + "]" )
         }
-        
+
         let myRange1 : ClosedRange<Int> = 0...5
         print( "Range in For Loop TWO" )
         for l : Int in myRange1
         {
             print( " Number is [" + String( l ) + "]" )
         }
-        
+
         print( "Test code END"            )
         print( "========================" )
         print( ""                         )
     }
-    
+
     /**
      *  Returns a salutation.
      *
@@ -210,7 +215,7 @@ This is my line 3.
     {
         return "Hello " + person + ", today is " + day + " and your age is " + String( age ) + "."
     }
-    
+
     /**
      *  Prints out a string.
      *
